c9937dcd59134de0327935f78ac7a70108f62ec8
Sandro Wymann <sandro.wymann@xappido.com>
2016-10-27 17:20:45 +0200

    initial OutputExtensions

---
+++ ConsoleUtils.Net.TestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleUtils.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUtils.Net.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "This is a blue line".ToConsole(ConsoleColor.Blue);
+            "This is a red line".ToConsole(ConsoleColor.Red);
+
+            var value = "Test";
+            $"This is a {value} Value".ToConsole(ConsoleColor.Cyan);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleUtils.Net/OutputExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUtils.Net
+{
+    public static class OutputExtensions
+    {
+        public static void ToConsole(this string output, ConsoleColor color = ConsoleColor.Gray, bool asLine = true)
+        {
+            var prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            if (asLine)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.Write(output);
+            }
+            Console.ForegroundColor = prevColor;
+        }
+    }
+}
