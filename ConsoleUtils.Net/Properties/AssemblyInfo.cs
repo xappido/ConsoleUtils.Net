999bf625455d248f492bf651aa193a986c3fabb0
Sandro Wymann <sandro.wymann@xappido.com>
2016-10-27 19:47:23 +0200

    update assembly info and nuspec

--- ConsoleUtils.Net/Properties/AssemblyInfo.cs
+++ ConsoleUtils.Net/Properties/AssemblyInfo.cs
@@ -6,13 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConsoleUtils.Net")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("A collection of console utilities for better output.")]
+[assembly: AssemblyCompany("xappido ag")]
 [assembly: AssemblyProduct("ConsoleUtils.Net")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright ©  2016 xappido ag")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
